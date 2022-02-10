85f4944c46c1cdd0c165757cf886e7aea89d2ed9
cristianhdz <cristian@acuasoft.com>
2022-02-10 17:38:16 -0500

    New views created

--- polls_app/urls.py
+++ polls_app/urls.py
@@ -4,5 +4,12 @@ from . import views
 
 
 urlpatterns = [
-    path(route="", view=views.index, name="index")
+    # /polls/
+    path(route="", view=views.index, name="index"),
+    # /polls/5/detail/
+    path("<int:question_id>/", view=views.detail, name="detail"),
+    # /polls/5/results/
+    path("<int:question_id>/results/", view=views.results, name="results"),
+    # /polls/5/vote/
+    path("<int:question_id>/vote/", view=views.vote, name="vote"),
 ]
