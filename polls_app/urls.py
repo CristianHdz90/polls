643cecc8a5b2b4750d5321535d2ff90063717b40
cristianhdz <cristian@acuasoft.com>
2022-02-12 11:23:02 -0500

    New template created and 'url' template tag used

--- polls_app/urls.py
+++ polls_app/urls.py
@@ -2,12 +2,14 @@ from unicodedata import name
 from django.urls import path
 from . import views
 
+app_name = "polls_app"
+
 
 urlpatterns = [
     # /polls/
     path(route="", view=views.index, name="index"),
     # /polls/5/detail/
-    path("<int:question_id>/", view=views.detail, name="detail"),
+    path("specific/<int:question_id>/", view=views.detail, name="detail"),
     # /polls/5/results/
     path("<int:question_id>/results/", view=views.results, name="results"),
     # /polls/5/vote/
