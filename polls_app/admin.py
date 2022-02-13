e89f2d94822a5f35d7ebe4d93a3a8edcaf5c2f4c
cristianhdz <cristian@acuasoft.com>
2022-02-13 18:18:25 -0500

    Django admin customized

--- polls_app/admin.py
+++ polls_app/admin.py
@@ -1,8 +1,32 @@
 from django.contrib import admin
 
+from .models import Choice, Question
 
-from .models import Question, Choice
+
+class ChoiceInline(admin.StackedInline):
+    """
+    This class allow creating Choices
+    at the same time when a question is created.
+    """
+
+    model = Choice
+    extra = 3  # Number of choices
 
 
-admin.site.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    """
+    Class to change how the Question
+    object is shown in the Admin interface.
+    """
+
+    fields = ["pub_date", "question_text"]
+    # Bind the choices to the questions
+    inlines = [ChoiceInline]
+
+    list_display = ("question_text", "pub_date", "was_published_recently")
+    list_filter = ["pub_date"]
+    search_fields = ["question_text", "pub_date"]
+
+
+admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
